cc7f8f757be9ab84b6ef6f9edca24e1b41bc047f
elenkalee <elena.lipukhina@gmail.com>
2021-05-18 10:46:50 +0300

    branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn200IfAmountIs1800() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        Assert.assertEquals(actual, expected);
+    }
+
+}
